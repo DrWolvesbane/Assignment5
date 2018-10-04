f38741f02b79aa926b5afda25da48ea8c16fcf3b
Magnuswolf3 <43730403+Magnuswolf3@users.noreply.github.com>
2018-10-04 13:45:44 +0200

    Iteration_7
    Decreased intensity to 0
    Added CameraFollow

---
+++ 2nd-Year-Game-Jam-master/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+	public GameObject Player;
+	Vector3 playerWatch;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerWatch = new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z - 20f);
+		transform.position = playerWatch;
+	}
+}
